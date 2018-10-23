cf3f7419e3eefb4b30cf00827f9c9c401953b58d
KyleLuoma <kluoma@gmail.com>
2018-10-23 10:29:27 -0400

    Created list.java

---
+++ src/main/java/com/kyleluoma/application/model/list.java
@@ -0,0 +1,17 @@
+package com.kyleluoma.application.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GenerationType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class list {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Integer Id;
+    
+    public Integer getId() {
+        return Id;
+    }
+}
