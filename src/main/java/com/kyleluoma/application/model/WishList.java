8fc1c0a0f1535b6532c86b6e4e97451e4e4c5f82
KyleLuoma <kluoma@gmail.com>
2018-10-23 10:32:59 -0400

    Added fields, accessors and mutators to WishList.java

---
+++ src/main/java/com/kyleluoma/application/model/WishList.java
@@ -0,0 +1,37 @@
+package com.kyleluoma.application.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GenerationType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class WishList {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Integer Id;
+    
+    private Integer userId;
+    private String title;
+    private String description;
+    
+    public Integer getId() {
+        return Id;
+    }
+    
+    public String getTitle() {
+        return title;
+    }
+    
+    public String getDescription() {
+        return description;
+    }
+    
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
