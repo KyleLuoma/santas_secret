f853bf8e7e3dbee0a9ecad93ce298d23fbe7ee34
KyleLuoma <kluoma@gmail.com>
2018-10-23 10:40:09 -0400

    Created DesiredItem Class

---
+++ src/main/java/com/kyleluoma/application/model/DesiredItem.java
@@ -0,0 +1,69 @@
+package com.kyleluoma.application.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GenerationType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class DesiredItem {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Integer id;
+    
+    private Integer wishListID;
+    private String title;
+    private String description;
+    private String URL;
+    private Boolean purchased;
+    private Integer purchasedByUserId;
+    private Enum priority;
+    
+    public Integer getId() {
+        return id;
+    }
+    
+    public String getTitle() {
+        return title;
+    }
+    
+    public String getDescription() {
+        return description;
+    }
+    
+    public String getURL() {
+        return URL;
+    }
+    
+    public Boolean getPurchase() {
+        return purchased;
+    }
+    
+    public Integer getPurchasedByUserId() {
+        return purchasedByUserId;
+    }
+    
+    public Enum getPriority() {
+        return priority;
+    }
+    
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
+    public void setURL(String URL) {
+        this.URL = URL;
+    }
+    
+    public void setPurchased(Boolean purchased) {
+        this.purchased = purchased;
+    }
+    
+    public void setPriorit(Enum priority) {
+        this.priority = priority;
+    }
+}
