33fa0d0216baa7c1efeee9f10b7251a959b6c591
KyleLuoma <kluoma@gmail.com>
2018-10-26 08:18:25 -0400

    Created DesiredItemRepository.java

---
+++ src/main/java/com/kyleluoma/application/repository/DesiredItemRepository.java
@@ -0,0 +1,9 @@
+package com.kyleluoma.application.repository;
+
+import com.kyleluoma.application.model.DesiredItem;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface DesiredItemRepository extends CrudRepository<User, Integer> {
+
+}
