1c50bad8813a60a0dd60095e4fb16f05d7bb26ac
KyleLuoma <kluoma@gmail.com>
2018-10-26 08:21:02 -0400

    Created UserGiftPoolRelationshipRepository.java

---
+++ src/main/java/com/kyleluoma/application/repository/UserGiftPoolRelationshipRepository.java
@@ -0,0 +1,9 @@
+package com.kyleluoma.application.repository;
+
+import com.kyleluoma.application.model.UserGiftPoolRelationship;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserGiftPoolRelationshipRepository extends CrudRepository<User, Integer> {
+
+}
