deba7c9bed9637052f5d3fb37238be31b8002c6f
KyleLuoma <kluoma@gmail.com>
2018-10-26 08:21:45 -0400

    Created WishListRepository.java

---
+++ src/main/java/com/kyleluoma/application/repository/WishListRepository.java
@@ -0,0 +1,9 @@
+package com.kyleluoma.application.repository;
+
+import com.kyleluoma.application.model.WishList;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface WishListRepository extends CrudRepository<User, Integer> {
+
+}
