8586c47be9fc6cf4971ece61939a465e1f3db387
KyleLuoma <kluoma@gmail.com>
2018-10-26 08:19:15 -0400

    Created GiftPoolRepository.java

---
+++ src/main/java/com/kyleluoma/application/repository/GiftPoolRepository.java
@@ -0,0 +1,9 @@
+package com.kyleluoma.application.repository;
+
+import com.kyleluoma.application.model.GiftPool;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface GiftPoolRepository extends CrudRepository<User, Integer> {
+
+}
