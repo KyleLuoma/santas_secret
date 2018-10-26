fb2a9951e852738af387425ff314baa7851c4d33
KyleLuoma <kluoma@gmail.com>
2018-10-26 08:20:14 -0400

    Created ItemPoolVisibilityRepository.java

---
+++ src/main/java/com/kyleluoma/application/repository/ItemPoolVisibilityRepository.java
@@ -0,0 +1,9 @@
+package com.kyleluoma.application.repository;
+
+import com.kyleluoma.application.model.ItemPoolVisibility;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ItemPoolVisibilityRepository extends CrudRepository<User, Integer> {
+
+}
