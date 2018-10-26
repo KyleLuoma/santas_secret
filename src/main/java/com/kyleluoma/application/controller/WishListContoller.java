c473546b2344b81574d65e7cfd949ee0834069d6
KyleLuoma <kluoma@gmail.com>
2018-10-26 08:55:36 -0400

    Created WishListContoller.java

---
+++ src/main/java/com/kyleluoma/application/controller/WishListContoller.java
@@ -0,0 +1,34 @@
+package com.kyleluoma.application.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.kyleluoma.application.model.WishList;
+import com.kyleluoma.application.repository.WishListRepository;
+
+@Controller
+@RequestMapping(path="/wish_list")
+public class WishListController {
+    @Autowired
+    private WishListRepository wishListRepository;
+
+    @GetMapping(path="/add")
+    public @ResponseBody String addNewWishList (@RequestParam Integer userId,
+                                                @RequestParam String title,
+                                                @RequestParam String description) {
+        User newWishList = new WishList();
+        newWishList.setUserId(userId);
+        newWishList.setTitle(title);
+        newWishList.setDescription(description);
+        return "Saved";
+    }
+
+    @GetMapping(path="/all")
+    public @ResponseBody Iterable<WishList> getAllWishLists() {
+        return wishListRepository.findAll();
+    }
+}
