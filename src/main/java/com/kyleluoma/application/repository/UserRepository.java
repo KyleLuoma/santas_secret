29e44652d25ae918c400bbabe5d974e470869e46
KyleLuoma <kluoma@gmail.com>
2018-10-29 13:09:28 -0400

    added findBy methods to UserRepository

--- src/main/java/com/kyleluoma/application/repository/UserRepository.java
+++ src/main/java/com/kyleluoma/application/repository/UserRepository.java
@@ -5,5 +5,12 @@ import com.kyleluoma.application.model.User;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<User, Integer> {
+  
+    User findByEmail(String email);
+    Iterable<User> findByFirstNameAndLastName(String firstName, String lastName);
+    Iterable<User> findByLastName(String lastName);
+    Iterable<User> findByFirstName(String firstName);
+    Iterable<User> findByUserName(String userName);
+    Iterable<User> findByUserNameAndHashedPassword(String userName, String hashedPassword);
 
 }
