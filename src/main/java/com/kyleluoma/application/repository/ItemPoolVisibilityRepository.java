4fc8b50616551c9090b6efbc5a093a068dd82a61
KyleLuoma <kluoma@gmail.com>
2018-10-29 13:06:11 -0400

    Added findBy methods to ItemPoolVisibilityRepository

--- src/main/java/com/kyleluoma/application/repository/ItemPoolVisibilityRepository.java
+++ src/main/java/com/kyleluoma/application/repository/ItemPoolVisibilityRepository.java
@@ -5,5 +5,13 @@ import com.kyleluoma.application.model.ItemPoolVisibility;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ItemPoolVisibilityRepository extends CrudRepository<ItemPoolVisibility, Integer> {
+    
+    Iterable<ItemPoolVisibility> findByVisId(Integer visId);
+    Iterable<ItemPoolVisibility> findByItemId(Integer itemId);
+    Iterable<ItemPoolVisibility> findByPoolId(Integer poolId);
+    Iterable<ItemPoolVisibility> findByItemIdAndVisible(Integer itemId, boolean visible);
+    Iterable<ItemPoolVisibility> findByPoolIdAndVisible(Integer poolId, boolean visible);
+    Iterable<ItemPoolVisibility> findByItemIdOrPoolId(Integer itemId, Integer poolId);
+    Iterable<ItemPoolVisibility> findByItemIdOrPoolIdAndVisible(Integer itmeId, Integer poolId, boolean visible);
 
 }
