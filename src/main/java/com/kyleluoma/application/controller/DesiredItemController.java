3238d3989e738cec73b8d66e022a727e8af14246
KyleLuoma <kluoma@gmail.com>
2018-10-29 21:51:42 -0400

    Updated DesiredItemController get methods     Updated getWishListDesiredItems()     Updated getItemPoolDesiredItems()

--- src/main/java/com/kyleluoma/application/controller/DesiredItemController.java
+++ src/main/java/com/kyleluoma/application/controller/DesiredItemController.java
@@ -1,5 +1,7 @@
 package com.kyleluoma.application.controller;
 
+import com.kyleluoma.application.model.ItemPoolVisibility;
+import com.kyleluoma.application.repository.ItemPoolVisibilityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,16 +20,17 @@ public class DesiredItemController {
     @Autowired
     private DesiredItemRepository desiredItemRepository;
 
+    @Autowired
+    private ItemPoolVisibilityRepository itemPoolVisibilityRepository;
+
     @GetMapping(path="/add")
-    public @ResponseBody String addNewDesiredItem (@RequestParam Integer wishListID,
-                                                   @RequestParam String title,
+    public @ResponseBody String addNewDesiredItem (@RequestParam String title,
                                                    @RequestParam String description,
                                                    @RequestParam String URL,
                                                    @RequestParam Boolean purchased,
                                                    @RequestParam Integer purchasedByUserId,
                                                    @RequestParam Enum priority) {
         DesiredItem newDesiredItem = new DesiredItem();
-        newDesiredItem.setWishListID(wishListID);
         newDesiredItem.setTitle(title);
         newDesiredItem.setDescription(description);
         newDesiredItem.setURL(URL);
@@ -45,13 +48,17 @@ public class DesiredItemController {
 
     @GetMapping(path="/wish_list_items")
     public @ResponseBody Iterable<DesiredItem> getWishListDesiredItems(Integer wishListId) {
-        Iterable<DesiredItem> allItems = desiredItemRepository.findAll();
-        ArrayList<DesiredItem> wishListItems = new ArrayList<>();
-        for(DesiredItem item: allItems) {
-            if(item.getWishListID() == wishListId) {
-                wishListItems.add(item);
+        return desiredItemRepository.findByWishListID(wishListId);
+    }
+
+    @GetMapping(path="/item_pool_items")
+    public @ResponseBody Iterable<DesiredItem> getItemPoolDesiredItems(Integer giftPoolId) {
+        ArrayList<Integer> itemIds = new ArrayList<>();
+        for(ItemPoolVisibility itemPoolVis : itemPoolVisibilityRepository.findByPoolId(giftPoolId)) {
+            if (itemPoolVis.getPoolId() == giftPoolId) {
+                itemIds.add(itemPoolVis.getItemId());
             }
         }
-        return wishListItems;
+        return desiredItemRepository.findAllById(itemIds);
     }
 }
