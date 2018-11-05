3ace31649a61ac4781f35523203f7facaec35419
KyleLuoma <kluoma@gmail.com>
2018-11-05 15:25:29 -0500

    Updated ItemQuestion.java

--- src/main/java/com/kyleluoma/application/model/ItemQuestion.java
+++ src/main/java/com/kyleluoma/application/model/ItemQuestion.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GenerationType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import java.time.Instant;
 
 @Entity
 public class ItemQuestion {
