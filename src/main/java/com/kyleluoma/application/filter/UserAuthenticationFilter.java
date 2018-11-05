12f289fcbb6190cb62b10f1e562f2ecd0441df3a
KyleLuoma <kluoma@gmail.com>
2018-11-05 13:14:50 -0500

    Updated UserAuthenticationFilter.java

--- src/main/java/com/kyleluoma/application/filter/UserAuthenticationFilter.java
+++ src/main/java/com/kyleluoma/application/filter/UserAuthenticationFilter.java
@@ -7,15 +7,16 @@ urlPatterns = {"/*"},
 initParams = {
     @WebInitParam(/*need to pass in session here*/)})
 public class UserAuthenticationFilter implements Filter {
-    init() {
+    public void init(FilterConfig config) throws ServletException {
     
     }
     
+    doFilter(ServletRequest request, ServletResponse response, FilterChain chain) 
+            throws IOException, ServletException {
+    
+    }
+        
     destroy() {
-    
-    }
-    
-    doFilter() {
     
     }
 }
