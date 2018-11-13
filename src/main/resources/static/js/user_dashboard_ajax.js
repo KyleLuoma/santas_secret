deef3c6d51e6b70499daa5cc59e8315cba56a7d9
KyleLuoma <kluoma@gmail.com>
2018-11-12 21:39:32 -0500

    Ajax call and pool table generation on user dashboard

--- src/main/resources/static/js/user_dashboard_ajax.js
+++ src/main/resources/static/js/user_dashboard_ajax.js
@@ -1,5 +1,9 @@
-
-
+/**
+ * Populates element with gift_pool_table id with table generating HTML containing
+ * gift pools that userId is a member of.
+ * @param userId user Id number associated with gift pools to appear in table
+ * @returns {string}
+ */
 function retrieveUserPools(userId) {
     var result = "";
     $.ajax({
@@ -8,7 +12,17 @@ function retrieveUserPools(userId) {
         data: {userId: userId},
         dataType: 'json',
         success: function(json) {
+            var table_string = "<table class='table'><tr></tr><th>Title:</th><th>Description:</th></tr>";
+            //var poolArray = JSON.parse(json);
+
+            for(var i = 0; i < json.length; i++) {
+
+                table_string = table_string + "<tr><td>" + json[i].poolTitle + "</td><td>" + json[i].poolDescription + "</td></tr>";
+            }
+
+            table_string = table_string + "</table>";
             document.getElementById("dump").innerHTML = JSON.stringify(json);
+            document.getElementById("gift_pool_table").innerHTML = table_string;
         },
         error: function() {
             console.log("Oops, something went wrong")
