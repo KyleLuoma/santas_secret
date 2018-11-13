bdb59f4ef298404f8f402f9ffdf319d324be7282
KyleLuoma <kluoma@gmail.com>
2018-11-13 14:54:28 -0500

    Added meta data fields to gift pool tables in user_dashboard

--- src/main/resources/static/js/user_dashboard_ajax.js
+++ src/main/resources/static/js/user_dashboard_ajax.js
@@ -12,20 +12,25 @@ function retrieveUserPools(userId) {
         data: {userId: userId},
         dataType: 'json',
         success: function(json) {
-            var table_string = "<table class='table'><tr></tr><th>Title:</th><th>Description:</th></tr>";
+            var table_string = "<table class='table'><tr></tr><th>Title:</th><th>Description:</th></tr>\n";
             //var poolArray = JSON.parse(json);
 
             for(var i = 0; i < json.length; i++) {
-
-                table_string = table_string + "<tr><td>" + json[i].poolTitle + "</td><td>" + json[i].poolDescription + "</td></tr>";
+                table_string += "<tr><td>" + json[i].poolTitle + "</td><td>" + json[i].poolDescription + "</td></tr>\n";
+                table_string += "  <tr>\n" + 
+                                "    <td id='users_in_pool" + json[i].id + "'></td>\n" + 
+                                "    <td id='lists_in_pool" + json[i].id + "'></td>\n" + 
+                                "    <td id='gifts_purchased_in_pool" + json[i].id + "'>\n" + 
+                                "    <td><a href='\\pool_view?poolId=" + json[i].id + "'>VIEW</a></td>\n" + 
+                                "  </tr>";
             }
-
-            table_string = table_string + "</table>";
+            table_string = table_string + "\n</table>";
             document.getElementById("dump").innerHTML = JSON.stringify(json);
             document.getElementById("gift_pool_table").innerHTML = table_string;
         },
         error: function() {
-            console.log("Oops, something went wrong")
+            console.log("Oops, something went wrong when trying to load gift pool data.");
+            document.getElementById("gift_pool_table").innerHTML = "Unable to load gift pool data.";
         }
     });
     return result;
