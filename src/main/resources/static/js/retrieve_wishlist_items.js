3f25104aca54ab9fc8b9a8deda6009908faf5bdd
EC2 Default User <ec2-user@ip-172-31-15-212.us-west-2.compute.internal>
2018-12-19 17:52:42 +0000

    updated wishlist static resources

--- src/main/resources/static/js/retrieve_wishlist_items.js
+++ src/main/resources/static/js/retrieve_wishlist_items.js
@@ -1,20 +1,21 @@
 retrieveWishListItems(1, "item_list_target");
 
 function retrieveWishListItems(wishListId, htmlElementId) {
+	/*
 	var mockWishListItemListJSON = 
   	"[" +
     "   {\"id\": \"1\", \"wishListID\": \"1\", \"title\": \"Lego Saturn V\", \"description\": \"A big Lego rocket set\", \"URL\": \"www.lego.com\", \"purchased\": \"false\", \"purchasedByUserId\": \"0\", \"priority\":\"medium\"}," +
     "   {\"id\": \"2\", \"wishListID\": \"1\", \"title\": \"Beer mug\", \"description\": \"Stainless steel beer mug\", \"URL\": \"www.beermugs.com\", \"purchased\": \"true\", \"purchasedByUserId\": \"4\", \"priority\":\"medium\"}," +
     "   {\"id\": \"3\", \"wishListID\": \"1\", \"title\": \"Book V\", \"description\": \"The Art of War by Sun Tzu\", \"URL\": \"www.books.com\", \"purchased\": \"false\", \"purchasedByUserId\": \"0\", \"priority\":\"medium\"}"  +
     "]";
-  
+  */
   $.ajax({
-  	url:"/echo/json",
-    //url:"/desired_item/wish_list_items",
-    type: "POST",
-    //type: "GET",
-    data: {json: mockWishListItemListJSON},
-    //data: {wishListId: wishListId},
+  	//url:"/echo/json",
+    url:"/desired_item/wish_list_items",
+    //type: "POST",
+    type: "GET",
+    //data: {json: mockWishListItemListJSON},
+    data: {wishListId: wishListId},
     datatype: "json",
     success: function(json) {
     	var table_string = "<table class='table'><tr></tr><th>Item:</th><th>Description:</th><th>Link:</th><th>Questions:</th></tr>";
